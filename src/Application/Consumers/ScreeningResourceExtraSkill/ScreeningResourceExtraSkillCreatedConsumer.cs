c193b55f9af06b6005ae823e160eb1927bba36be
OctavioCuellar <octavio.cuellar@atos.net>
2023-05-08 15:20:25 -0600

    consumer de ScreeningResourceExtraSkillCreated agregado

---
+++ src/Application/Consumers/ScreeningResourceExtraSkill/ScreeningResourceExtraSkillCreatedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Atos.Core.EventsDTO;
+using Domain.Entities;
+using MassTransit;
+
+namespace Application.Consumers.ScreeningResourceExtraSkill;
+
+public class ScreeningResourceExtraSkillCreatedConsumer : IConsumer<ScreeningResourceExtraSkillCreated>
+{
+    private readonly IRepositoryAsync<ResourceExtraSkills> _repository;
+
+    public ScreeningResourceExtraSkillCreatedConsumer(IRepositoryAsync<ResourceExtraSkills> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Consume(ConsumeContext<ScreeningResourceExtraSkillCreated> context)
+    {
+        var message = context.Message;
+        var resourceExtraSkills = new ResourceExtraSkills
+        {
+            Title = message.Title,
+            ResourceId = message.ResourceId,
+            ExperienceOverallTypeTag = message.ExperienceOverallTypeTag,
+            BriefDescription = message.BriefDescription,
+            Point = message.ScreeningPoint,
+            IsApproved = message.IsApproved
+        };
+
+        await _repository.AddAsync(resourceExtraSkills);
+    }
+}
